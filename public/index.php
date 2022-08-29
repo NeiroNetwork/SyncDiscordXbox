0391720d4033683db3560c05694a0862f9ed61f8
Nerahikada <hkshirorom@yahoo.co.jp>
2022-08-30 02:41:04 +0900

    Xbox Liveの認証も完成

--- public/index.php
+++ public/index.php
@@ -19,15 +19,24 @@ $dotenv->required(["XBL_CLIENT_ID",
 
 session_start();
 
-if(isset($_GET["error"])){
+if(isset($_GET["error"])) {
 	header("Location: discord:///channels/" . $_ENV["DISCORD_SERVER_ID"]);
 	exit;
 }
 
-if(!isset($_SESSION["discord_id"])){
+if(empty($_SESSION["discord_id"]) || !is_string($_SESSION["discord_id"])){
 	$result = require_once dirname(__DIR__) . "/src/discord_oauth.php";
-	$_SESSION["discord_id"] = $result !== 1 ?: exit;
+	$_SESSION["discord_id"] = $result === 1 ? exit : $result;
 }
-
-echo "Done: Discord";
-// TODO: ユーザーがDiscordサーバーに参加しているかチェック
+if(!empty($_SESSION["discord_id"])){
+	$result = require_once dirname(__DIR__) . "/src/xboxlive_oauth.php";
+	$_SESSION["xuid"] = $result === 1 ? exit : $result[0];
+	$_SESSION["gamertag"] = $result[1];
+}
+if(is_string($_SESSION["discord_id"]) && is_string($_SESSION["xuid"]) && is_string($_SESSION["gamertag"])){
+	echo "<pre>";
+	var_dump($_SESSION);
+	session_destroy();
+}else{
+	echo "Something went wrong!";
+}
