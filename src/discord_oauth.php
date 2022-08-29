7b6b57673d4ad7a9b5b7fae6449b4b9a8a2f80ee
Nerahikada <hkshirorom@yahoo.co.jp>
2022-08-30 02:41:52 +0900

    publicフォルダーで作業してたから他のファイルがトラックされてなかった

---
+++ src/discord_oauth.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
+use Wohali\OAuth2\Client\Provider\Discord;
+use Wohali\OAuth2\Client\Provider\DiscordResourceOwner;
+
+$provider = new Discord([
+	"clientId" => $_ENV["DISCORD_CLIENT_ID"],
+	"clientSecret" => $_ENV["DISCORD_CLIENT_SECRET"],
+	"redirectUri" => $_ENV["DISCORD_REDIRECT_URI"],
+]);
+
+if(isset($_GET["code"], $_GET["state"], $_SESSION["oauth2state"]) && $_GET["state"] === $_SESSION["oauth2state"]){
+	try{
+		$token = $provider->getAccessToken("authorization_code", ["code" => $_GET["code"]]);
+
+		// https://github.com/wohali/oauth2-discord-new/issues/10#issuecomment-436761132
+		$requestUrl = $provider->getResourceOwnerDetailsUrl($token) . "/guilds";
+		$guildsRequest = $provider->getAuthenticatedRequest("GET", $requestUrl, $token);
+		/** @var array $guilds */
+		$guilds = $provider->getParsedResponse($guildsRequest);
+
+		$guildIds = array_map(fn(array $guild) => $guild["id"], $guilds);
+		if(!in_array($_ENV["DISCORD_SERVER_ID"], $guildIds, true)) {
+			echo "First you need to join Neiro Network's Discord server!";
+			die;
+		}
+
+		/** @var DiscordResourceOwner $user */
+		$user = $provider->getResourceOwner($token);
+		return $user->getId();
+	}catch(IdentityProviderException){}
+}
+
+unset($_SESSION["oauth2state"]);
+$authorizationUrl = $provider->getAuthorizationUrl(["scope" => ["identify", "guilds"]]);
+$_SESSION['oauth2state'] = $provider->getState();
+header("Location: $authorizationUrl");
---
+++ src/xboxlive_oauth.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+use BigPino67\OAuth2\XBLive\Client\Provider\Profiles\Models\Profile;
+use BigPino67\OAuth2\XBLive\Client\Provider\Profiles\ProfilesProvider;
+use BigPino67\OAuth2\XBLive\Client\Provider\XBLive;
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
+
+$provider = new XBLive([
+	"clientId" => $_ENV["XBL_CLIENT_ID"],
+	"clientSecret" => $_ENV["XBL_CLIENT_SECRET"],
+	"redirectUri" => $_ENV["XBL_REDIRECT_URI"],
+]);
+
+if(isset($_GET["code"], $_GET["state"], $_SESSION["oauth2state"]) && $_GET["state"] === $_SESSION["oauth2state"]){
+	try{
+		$token = $provider->getAccessToken("authorization_code", ["code" => $_GET["code"]]);
+		$token = $provider->getXstsToken($provider->getXasuToken($token));
+		$profiles = new ProfilesProvider($token);
+		/** @var Profile $profile */
+		$profile = $profiles->getLoggedUserProfile();
+
+		return [$profile->getId(), $profile->getSettings()->getGamertag()];
+		var_dump($profile->getId(), $profile->getSettings()->getGamertag());
+	}catch(IdentityProviderException){}
+}
+
+unset($_SESSION["oauth2state"]);
+$authorizationUrl = $provider->getAuthorizationUrl(["scope" => "xbl.signin"]);
+$_SESSION['oauth2state'] = $provider->getState();
+header("Location: $authorizationUrl");
