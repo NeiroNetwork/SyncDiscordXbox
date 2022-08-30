fb85852a9c0ff16297ab0bdd4cd0331091e09797
Nerahikada <hkshirorom@yahoo.co.jp>
2022-08-30 15:42:11 +0900

    ボットの挙動について明記

--- src/sync_accounts.php
+++ src/sync_accounts.php
@@ -5,9 +5,10 @@ declare(strict_types=1);
 use RestCord\DiscordClient;
 
 $discord = new DiscordClient(["token" => $_ENV["DISCORD_BOT_TOKEN"]]);
+// 既についているロールを消してしまうが、一度に変更した方が速い…
 $discord->guild->modifyGuildMember([
-	"guild.id" => $_ENV["DISCORD_GUILD_ID"],
-	"user.id" => $_SESSION["discord_id"],
+	"guild.id" => (int) $_ENV["DISCORD_GUILD_ID"],
+	"user.id" => (int) $_SESSION["discord_id"],
 	"nick" => $_SESSION["gamertag"],
-	"roles" => [$_ENV["MEMBER_ROLE_ID"]],
+	"roles" => [(int) $_ENV["MEMBER_ROLE_ID"]],
 ]);
