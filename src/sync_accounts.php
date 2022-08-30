96f6ecb673b3c789171861953660b9eb19099bc6
Nerahikada <hkshirorom@yahoo.co.jp>
2022-08-30 23:49:32 +0900

    データベースで失敗した時のハンドリングを追加

--- src/sync_accounts.php
+++ src/sync_accounts.php
@@ -6,28 +6,33 @@ use GuzzleHttp\Command\Exception\CommandClientException;
 use Illuminate\Database\Capsule\Manager as Capsule;
 use RestCord\DiscordClient;
 
+$guildId = (int) $_ENV["DISCORD_GUILD_ID"];
+$roleId = (int) $_ENV["MEMBER_ROLE_ID"];
+$userId = (int) $_SESSION["discord_id"];
+$xuid = (int) $_SESSION["xuid"];
+$gamertag = $_SESSION["gamertag"];
+
 $discord = new DiscordClient(["token" => $_ENV["DISCORD_BOT_TOKEN"]]);
 try{
-	$discord->guild->addGuildMemberRole([
-		"guild.id" => (int) $_ENV["DISCORD_GUILD_ID"],
-		"user.id" => (int) $_SESSION["discord_id"],
-		"role.id" => (int) $_ENV["MEMBER_ROLE_ID"],
-	]);
-	$discord->guild->modifyGuildMember([
-		"guild.id" => (int) $_ENV["DISCORD_GUILD_ID"],
-		"user.id" => (int) $_SESSION["discord_id"],
-		"nick" => $_SESSION["gamertag"],
-	]);
+	$discord->guild->addGuildMemberRole(["guild.id" => $guildId, "user.id" => $userId, "role.id" => $roleId]);
+	$discord->guild->modifyGuildMember(["guild.id" => $guildId, "user.id" => $userId, "nick" => $gamertag]);
 }catch(CommandClientException $exception){
 	session_destroy();
 	generatePage("認証に失敗しました", "Discordアカウントとの連携中にエラーが発生しました。最初からやり直してください。");
 }
 
-require_once dirname(__DIR__) . "/src/connect_database.php";
-Capsule::table("discord_xuid_map")->upsert([
-	"discord_id" => $_SESSION["discord_id"],
-	"xuid" => $_SESSION["xuid"],
-], "discord_id");
+try{
+	require_once dirname(__DIR__) . "/src/connect_database.php";
+	Capsule::table("discord_xuid_map")->upsert(["discord_id" => $userId, "xuid" => $xuid], "discord_id");
+}catch(PDOException){
+	session_destroy();
+	$discord->guild->removeGuildMemberRole(["guild.id" => $guildId, "user.id" => $userId, "role.id" => $roleId]);
+	$discord->guild->modifyGuildMember(["guild.id" => $guildId, "user.id" => $userId, "nick" => ""]);
+	generatePage(
+		"認証に失敗しました",
+		"データベースに内部エラーが発生しました。しばらく待ってから再度お試しください。解決しない場合はお問い合わせください。"
+	);
+}
 
 session_destroy();
 generatePage("認証に成功しました", "アカウントの連携が完了しました。安全にこのページを閉じることができます。");
