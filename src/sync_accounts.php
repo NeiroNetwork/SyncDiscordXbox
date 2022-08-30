5c5f03388d4c35e49b935394bef413b233974dcd
Nerahikada <hkshirorom@yahoo.co.jp>
2022-08-30 11:34:57 +0900

    ニックネームも変更するように

--- src/sync_accounts.php
+++ src/sync_accounts.php
@@ -5,8 +5,9 @@ declare(strict_types=1);
 use RestCord\DiscordClient;
 
 $discord = new DiscordClient(["token" => $_ENV["DISCORD_BOT_TOKEN"]]);
-$discord->guild->addGuildMemberRole([
+$discord->guild->modifyGuildMember([
 	"guild.id" => $_ENV["DISCORD_GUILD_ID"],
 	"user.id" => $_SESSION["discord_id"],
-	"role.id" => $_ENV["MEMBER_ROLE_ID"],
+	"nick" => $_SESSION["gamertag"],
+	"roles" => [$_ENV["MEMBER_ROLE_ID"]],
 ]);
