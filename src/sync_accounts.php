afeaf11acc909feb9696c5a80fbc334cd8dfa45a
Nerahikada <hkshirorom@yahoo.co.jp>
2022-08-30 11:03:52 +0900

    ロールを付与するコードを書いた

--- src/sync_accounts.php
+++ src/sync_accounts.php
@@ -2,3 +2,11 @@
 
 declare(strict_types=1);
 
+use RestCord\DiscordClient;
+
+$discord = new DiscordClient(["token" => $_ENV["DISCORD_BOT_TOKEN"]]);
+$discord->guild->addGuildMemberRole([
+	"guild.id" => $_ENV["DISCORD_GUILD_ID"],
+	"user.id" => $_SESSION["discord_id"],
+	"role.id" => $_ENV["MEMBER_ROLE_ID"],
+]);
